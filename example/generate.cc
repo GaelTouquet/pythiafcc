927bc402986fe56441f7339485936e6e60dd00ff
Gael <gael.touquet@etu.univ-lyon1.fr>
2015-06-19 13:44:28 +0200

    updated example/genereate

--- example/generate.cc
+++ example/generate.cc
@@ -43,19 +43,25 @@ int main(){
   writer.registerForWrite<GenVertexCollection>("GenVertex");
   writer.registerForWrite<GenJetCollection>("GenJet");
 
-  unsigned nevents=5000;
+  unsigned nevents=300000;
 
   // Generator. Process selection. LHC initialization. Histogram.
   Pythia8::Pythia pythia;
   pythia.readString("Beams:idA = 11");
   pythia.readString("Beams:idB = -11");
 
-  pythia.readString("Beams:eCM = 91.");
-  pythia.readString("WeakSingleBoson:all = on");
-  pythia.readString("WeakZ0:gmZmode = 0");
-  pythia.readString("PhaseSpace:pTHatMin = 20.");
+  pythia.readString("Beams:eCM = 240.");
+  // pythia.readString("WeakSingleBoson:ffbar2gmZ = on");
+  // pythia.readString("23:mMin = 30");
+  // pythia.readString("22:mMin = 30");
+  // pythia.readString("HiggsSM:ffbar2HZ = on");
+  pythia.readString("WeakDoubleBoson:ffbar2WW = on");
+  // pythia.readString("PhaseSpace:pTHatMin = 20.");
+  // pythia.readString("24:onMode = off");
+  // pythia.readString("23:onIfMatch = 13 13");
+  // pythia.readString("24:onIfMatch = 15 16");
   // pythia.readString("23:onMode = off");
-  // pythia.readString("23:onIfMatch = 1 1");
+  // pythia.readString("23:onIfMatch = 13 13");
   pythia.init();
 
   // pythia.readString("Beams:eCM = 180.");
@@ -148,7 +154,6 @@ int main(){
       core.P4.Pz = hepmcptc->momentum().pz();
       core.P4.Mass = hepmcptc->momentum().m();
       // hepmcptc->print();
-
       if(core.Status==1) {
 	input_particles.push_back( fastjet::PseudoJet(hepmcptc->momentum().px(),
 						      hepmcptc->momentum().py(),
@@ -192,12 +197,11 @@ int main(){
       core.P4.Pz = jets[i].pz();
       core.P4.Mass = jets[i].m();    
     }
-
     writer.writeEvent();
     store.next();
   }
-
+  
   writer.finish();
-
+  pythia.stat();
   return 0;
 }
